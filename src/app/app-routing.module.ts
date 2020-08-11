900b06db5a20218ccc2b0d04b27a64b106293bc7
hollman <hollmanerojas@gmail.com>
2020-08-10 19:42:28 -0500

    VERSION1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,78 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'clases',
+    pathMatch: 'full'
+  },
+  {
+    path: 'clases',
+    loadChildren: () => import('./clases/clases.module').then( m => m.ClasesPageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'contactenos',
+    pathMatch: 'full'
+  },
+  {
+    path: 'contactenos',
+    loadChildren: () => import('./contactenos/contactenos.module').then( m => m.ContactenosPageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'crear-cuenta',
+    pathMatch: 'full'
+  },
+  {
+    path: 'crear-cuenta',
+    loadChildren: () => import('./crear-cuenta/crear-cuenta.module').then( m => m.CrearCuentaPageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'perfil-usuario',
+    pathMatch: 'full'
+  },
+  {
+    path: 'perfil-usuario',
+    loadChildren: () => import('./perfil-usuario/perfil-usuario.module').then( m => m.PerfilUsuarioPageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'quienes-somos',
+    pathMatch: 'full'
+  },
+  {
+    path: 'quienes-somos',
+    loadChildren: () => import('./quienes-somos/quienes-somos.module').then( m => m.QuienesSomosPageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'simulador-clases',
+    pathMatch: 'full'
+  },
+  {
+    path: 'simulador-clases',
+    loadChildren: () => import('./simulador-clases/simulador-clases.module').then( m => m.SimuladorClasesPageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'videos',
+    pathMatch: 'full'
+  },
+  {
+    path: 'videos',
+    loadChildren: () => import('./videos/videos.module').then( m => m.VideosPageModule)
   }
 ];
 
